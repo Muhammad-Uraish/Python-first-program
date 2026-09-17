d215d589758388dc3fc09fbd22f68267c8a73716
Muhammad Uraish <sabeehsabeehurrehman@gmail.com>
2026-09-17 11:05:26 +0500

    Add files via upload

---
+++ python syntax.py
@@ -0,0 +1,45 @@
+# 1. Print
+print("Hello World!")
+
+# 2. Variables
+name = "Uraish"
+age = 20
+
+print(name)
+print(age)
+
+# 3. Taking input
+city = input("Enter your city: ")
+print("You live in", city)
+
+# 4. If-else
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are under 18")
+
+# 5. Arithmetic
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# 6. For loop
+for i in range(5):
+    print(i)
+
+# 7. While loop
+count = 1
+
+while count <= 5:
+    print(count)
+    count = count + 1
+
+# 8. Function
+def greet():
+    print("Welcome to Python!")
+
+greet()
